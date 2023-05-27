360b541b39f20ae0f94abeef1ec65fad24d91295
Vaibhav Chimalgi <vchimu20@gmail.com>
2023-05-28 00:43:02 +0530

    new tab error fix

--- scripts/background.js
+++ scripts/background.js
@@ -15,13 +15,14 @@ async function main() {
   // When a tab is updated, check if it is blacklisted, and close it if so.
   chrome.tabs.onUpdated.addListener(async (tabId) => {
     var mode = await getMode();
-    if (mode!=='ruthless') return;
     var tab = await chrome.tabs.get(tabId);
-    if(tab.url){
+    if (mode!=='ruthless') return;
+    if(tab.url=='chrome://newtab/') {
+      return;
+    }
     if (!isUrlBlacklisted(tab.url)) return;
     try { if (tab && tab.id) await chrome.tabs.remove(tab.id); }
     catch (e) { console.error(e); }
-    }
   });
 }
 main();
